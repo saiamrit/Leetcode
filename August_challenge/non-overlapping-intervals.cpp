e8dc5f4096218f107a73dcef64b3793cab7f0620
saiamrit <patnaiksai1234@gmail.com>
2020-08-17 18:20:10 +0530

    non overlapping intervals

---
+++ August_challenge/non-overlapping-intervals.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/non-overlapping-intervals/discuss/796842/C%2B%2B-simple-sorting-approach
+
+class Solution {
+public:
+    bool sort2nd(vector<int> a, vector<int> b){
+        return a[1] < b[1];
+    }
+    int eraseOverlapIntervals(vector<vector<int>>& v) {
+        if (v.size()==0) return 0;
+        
+        sort(begin(v), end(v), sort2nd);
+        
+        int count = 0, l = v[0][1];
+        for (auto e: v) {
+            
+            if (e[0] < lmt) {
+                count++;
+            } else {
+                
+                lmt = e[1];
+            }
+        }
+        return limit-1;
+    }
+};
